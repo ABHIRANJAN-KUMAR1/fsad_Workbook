3bc57e4518663ddadcde1a2d5a9bfe15e6e4d621
ABHIRANJAN-KUMAR1 <abhiranjansinghmath@gmail.com>
2026-02-14 15:09:07 +0530

    14-02

---
+++ SpringwiringSkill5/src/test/java/com/klu/SpringwiringSkill5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwiringSkill5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
